5efee522934346caca80b227740e19fbf814e730
aamirmukaram <aamirmukaram@gmail.com>
2020-07-31 13:38:48 +0200

    Fixed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,21 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {FirebaseAppConfig} from '@angular/fire';
+
+const firebase: FirebaseAppConfig = {
+  apiKey: 'AIzaSyAsVKTcPZ0moW2W10HhDb2KW_w6ENtM0T4',
+  authDomain: 'craftnote-web-challenge.firebaseapp.com',
+  databaseURL: 'https://craftnote-web-challenge.firebaseio.com',
+  projectId: 'craftnote-web-challenge',
+  storageBucket: 'craftnote-web-challenge.appspot.com',
+  messagingSenderId: '926271509569',
+  appId: '1:926271509569:web:bf11e89892f3bb73fc62c0'
+};
+
 export const environment = {
-  production: false
+  production: false,
+  firebase
 };
 
 /*
