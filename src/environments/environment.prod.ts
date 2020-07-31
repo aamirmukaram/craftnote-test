1e47cc36631a0dbfb0fd0c9d475aa020b25ead52
aamirmukaram <aamirmukaram@gmail.com>
2020-07-31 18:21:38 +0200

    Added prod configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+import {FirebaseAppConfig} from '@angular/fire';
+
+const firebase: FirebaseAppConfig = {
+  apiKey: 'AIzaSyAsVKTcPZ0moW2W10HhDb2KW_w6ENtM0T4',
+  authDomain: 'craftnote-web-challenge.firebaseapp.com',
+  databaseURL: 'https://craftnote-web-challenge.firebaseio.com',
+  projectId: 'craftnote-web-challenge',
+  storageBucket: 'craftnote-web-challenge.appspot.com',
+  messagingSenderId: '926271509569',
+  appId: '1:926271509569:web:bf11e89892f3bb73fc62c0'
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebase
 };
